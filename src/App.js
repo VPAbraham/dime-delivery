31debfbe363c789ff8d84c354d8a194483c974da
Victor Abraham <vpa4565@gmail.com>
2020-08-11 19:15:50 -0600

    Fill out form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './assets/canna.png';
+import logo from './assets/delivery-guy.png';
 import './App.scss';
+import Form from './components/Form/Form';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="home-heading">
+        <h1>Dimebags Delivery</h1>
+        <h3>Delivery Drivers Wanted</h3>
+        <p>Fill out the below form and we will get in touch regarding you driving around with weed in your ass.</p>
+      </div>
+      <img src={logo} className="logo" alt="logo" />
+      <div className='form-container'>
+        <Form />
+      </div>
     </div>
   );
 }
